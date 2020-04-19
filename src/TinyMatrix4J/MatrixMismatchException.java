2bef51b17f3f58ce89c0b507223b407d18639b08
Anirudh Giri <anirudhges@gmail.com>
2020-04-19 22:58:33 +0530

    Add JSDoc

--- src/TinyMatrix4J/MatrixMismatchException.java
+++ src/TinyMatrix4J/MatrixMismatchException.java
@@ -1,10 +1,10 @@
 package TinyMatrix4J;
 
+/**
+ *Exception that is thrown when a matrix operation is being attempted between two incompatible matrices
+ */
 public class MatrixMismatchException extends Exception{
 
-    /**
-     *
-     */
     private static final long serialVersionUID = 1L;
     
     MatrixMismatchException(String s){
